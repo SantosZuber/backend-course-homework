87653d13c56ce197161ff4cd74235aeb6a338567
Santos Zuberbuhler <santoszuberbuhler2003@protonmail.com>
2022-08-30 18:46:45 -0300

    manejos de archivos finish

--- manejo-de-archivos/app.js
+++ manejo-de-archivos/app.js
@@ -1,45 +1,68 @@
-const fs = require('fs');
-const arr = [];
-class Contenedor {
-    constructor() {
+const fs = require("fs");
+let reading = [];
+let idCounter;
 
-    }
-    async save(obj) {
-        arr.push(obj)
-        console.log(arr)
-        for (let i = 0; i <= arr.length; i++) {
-            await fs.appendFile("productos.txt", arr[i].testprop, (err) => { //Problema al recibir un objeto.
-                if (err) {
-                    console.log("err")
-                }
-            })
-        }
-    }
-    getById(num) {
-
-    }
-    async getAll() {
-        let array = [];
-        await fs.readFile("productos.txt", "utf-8", (err, data) => {
-            if (err) {
-                console.log("err")
-            } else {
-                array.push(data)
-                return array;
-            }
-        });
-    }
-    deleteById() {
-
-    }
-    deleteAll() {
-
-    }
+try {
+  JSON.parse(fs.readFileSync("productos.json", "utf-8"));
+} catch {
+  fs.writeFileSync("productos.json", JSON.stringify(reading));
 }
 
+class Contenedor {
+  constructor() {}
+  save(obj) {
+    let lastElement = reading[reading.length - 1];
+    console.log(lastElement);
+    if (reading.length > 0) {
+      idCounter = lastElement.id + 1;
+    } else {
+      idCounter = 1;
+    }
+    obj.id = idCounter;
+    reading = JSON.parse(fs.readFileSync("productos.json", "utf-8"));
+    reading.push(obj);
+    fs.writeFileSync("productos.json", JSON.stringify(reading));
+    return idCounter;
+  }
+  getById(id) {
+    reading = JSON.parse(fs.readFileSync("productos.json", "utf-8"));
+    reading.filter((e) => {
+      if (e.id == id) {
+        return e;
+      } else {
+        return null;
+      }
+    });
+  }
+  getAll() {
+    reading = JSON.parse(fs.readFileSync("productos.json", "utf-8"));
+    return reading;
+  }
+  deleteById(id) {
+    reading = JSON.parse(fs.readFileSync("productos.json", "utf-8"));
+    reading.forEach((e) => {
+      if (e.id == id) {
+        let index = reading.indexOf(e);
+        reading.splice(index, 1);
+        console.log(reading);
+        return;
+      } else {
+        return;
+      }
+    });
+    fs.writeFileSync("productos.json", JSON.stringify(reading));
+  }
+  deleteAll() {
+    reading = JSON.parse(fs.readFileSync("productos.json", "utf-8"));
+    reading.splice(0, reading.length);
+    fs.writeFileSync("productos.json", JSON.stringify(reading));
+  }
+}
 let contenedor = new Contenedor();
-contenedor.save({ testprop: "text" });
-contenedor.getById();
-contenedor.getAll();
-contenedor.deleteById();
-contenedor.deleteAll();
+//contenedor.deleteAll();
+//console.log(contenedor.save({ title: "test", price: 100, thumbnail: null }));
+//console.log(contenedor.save({ title: "test", price: 100, thumbnail: null }));
+//console.log(contenedor.save({ title: "test", price: 100, thumbnail: null }));
+//console.log(contenedor.getById(1));
+//console.log(contenedor.getAll());
+//contenedor.deleteById(2);
